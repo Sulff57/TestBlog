756343fdffe10a075dfce966334351fa3cc29e0d
Sulff <valo.brice@gmail.com>
2019-05-01 00:38:55 +0200

    1) Creation fichier html_parts/charger_CDN_bootstrap.html 2) chargement bootstrap sur index.php / inscription.php 3) amélioration du formulaire inscription.php grâce a ce dernier

--- index.php
+++ index.php
@@ -5,92 +5,100 @@
     <meta charset="utf-8">
     <meta name="robots" content="noindex" />
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Affichage des billets</title>
 </head>
 
-<?php
+<body>
+  <div class="container"><?php
 
-$nbBilletsAffiches = 6;
-require('connexion_bdd.php');
-require('afficher_billets.php');
-require('verif_session.php');
-?>
+    $nbBilletsAffiches = 6;
+    require('connexion_bdd.php');
+    require('afficher_billets.php');
+    require('verif_session.php');
+    ?>
 
-<header>
-<?php include('html_parts/header.php');?>
-</header>
+    <header class="row">
+      <div class="col-lg-12">
+        <?php include('html_parts/header.php');?>
+      </div>
+    </header>
 
-<?php
+    <?php
 
-if (!empty($_GET['page']))
-{
-  $pageAffichee = $_GET['page'];
-}
-else
-{
-  $pageAffichee = 1;
-}
+    if (!empty($_GET['page']))
+    {
+      $pageAffichee = $_GET['page'];
+    }
+    else
+    {
+      $pageAffichee = 1;
+    }
 
 
-// Comptage nombre billets
-try {
-  $nombreBillets = $db_session->query(
-    '
-    SELECT COUNT(*) AS nb_billets FROM billets
-    '
-  );
-  if (!$nombreBillets) {
-    throw new Exception('La requête est nulle.');
-  }
-} catch (Exception $e) {
-  exit('Erreur d\'exécution de la requête SQL. <br/>
-  Message :<br/>' .
-  $e->getMessage());
-}
+    // Comptage nombre billets
+    try {
+      $nombreBillets = $db_session->query(
+        '
+        SELECT COUNT(*) AS nb_billets FROM billets
+        '
+      );
+      if (!$nombreBillets) {
+        throw new Exception('La requête est nulle.');
+      }
+    } catch (Exception $e) {
+      exit('Erreur d\'exécution de la requête SQL. <br/>
+      Message :<br/>' .
+      $e->getMessage());
+    }
 
-// Calcul du nombre de pages nécessaires pour afficher <x> billets par page
-$nombreBillets = $nombreBillets->fetch();
-$nombreBillets = $nombreBillets['nb_billets'];
+    // Calcul du nombre de pages nécessaires pour afficher <x> billets par page
+    $nombreBillets = $nombreBillets->fetch();
+    $nombreBillets = $nombreBillets['nb_billets'];
 
-// calcul du nombre de pages pouvant afficher les <x> billets souhaités
-$nbPagesCompletes = intval($nombreBillets / $nbBilletsAffiches);
-// s'il reste des billets à afficher, on ajoute une page
-$ajouterPage = boolval($nombreBillets % $nbBilletsAffiches);
+    // calcul du nombre de pages pouvant afficher les <x> billets souhaités
+    $nbPagesCompletes = intval($nombreBillets / $nbBilletsAffiches);
+    // s'il reste des billets à afficher, on ajoute une page
+    $ajouterPage = boolval($nombreBillets % $nbBilletsAffiches);
 
-$nombrePages = $nbPagesCompletes + $ajouterPage;
+    $nombrePages = $nbPagesCompletes + $ajouterPage;
 
-// Calcul de l'intervalle minimum des billets à afficher
-$minInterval = ($nbBilletsAffiches * ($pageAffichee - 1));
+    // Calcul de l'intervalle minimum des billets à afficher
+    $minInterval = ($nbBilletsAffiches * ($pageAffichee - 1));
 
-// Affiche tous les billets sur la page
-try {
-  $req = $db_session->prepare('
-  SELECT id, titre, contenu,
-  DATE_FORMAT(date_creation, "%d/%m/%Y") AS date_jour,
-  DATE_FORMAT(date_creation, "%Hh%imin%ss") AS date_heure
-  FROM billets
-  LIMIT :minInterval, :nbBilletsAffiches;
-  '
-  );
-  $req->bindValue(':minInterval', $minInterval, PDO::PARAM_INT);
-  $req->bindValue(':nbBilletsAffiches', $nbBilletsAffiches, PDO::PARAM_INT);
-  $req->execute();
+    // Affiche tous les billets sur la page
+    try {
+      $req = $db_session->prepare('
+      SELECT id, titre, contenu,
+      DATE_FORMAT(date_creation, "%d/%m/%Y") AS date_jour,
+      DATE_FORMAT(date_creation, "%Hh%imin%ss") AS date_heure
+      FROM billets
+      LIMIT :minInterval, :nbBilletsAffiches;
+      '
+      );
+      $req->bindValue(':minInterval', $minInterval, PDO::PARAM_INT);
+      $req->bindValue(':nbBilletsAffiches', $nbBilletsAffiches, PDO::PARAM_INT);
+      $req->execute();
 
-  if (!$req) {
-    throw new Exception('La requête est nulle.');
-  }
-} catch (Exception $e) {
-  exit('Erreur d\'exécution de la requête SQL. <br/>
-  Message :<br/>' .
-  $e->getMessage());
-}
+      if (!$req) {
+        throw new Exception('La requête est nulle.');
+      }
+    } catch (Exception $e) {
+      exit('Erreur d\'exécution de la requête SQL. <br/>
+      Message :<br/>' .
+      $e->getMessage());
+    }
 
-?>
-<section><?php
-afficher_billets($req);
-?></section><?php
+    ?>
+    <section><?php
+    afficher_billets($req);
+    ?></section><?php
 
-for ($x = 0; $x<=$nombrePages; $x++)
-{
-  echo('<a href="index.php?page=' . $x . '">' . $x . '</a>');
-}
+    for ($x = 0; $x<=$nombrePages; $x++)
+    {
+      echo('<a href="index.php?page=' . $x . '">' . $x . '</a>');
+    }
+    ?>
+  </div>
+<?php require('html_parts/charger_CDN_bootstrap.html');
+</body>
--- inscription.php
+++ inscription.php
@@ -5,19 +5,30 @@
     <meta charset="utf-8">
     <meta name="robots" content="noindex" />
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Page d'inscription</title>
 </head>
 
-<form action="inscription_post.php" method="post">
-  <p>
-    <label for="pseudo">Pseudo :</label><br/>
-    <input type="text" name="pseudo" id="pseudo" /><br/>
-    <label for="mail">Email :</label><br/>
-    <input type="text" name="mail" id="mail" /><br/>
-    <label for="password">Mot de passe :</label><br/>
-    <input type="password" name="password" id="password" /><br/>
-    <label for="password_check">Répétez le mot de passe :</label><br/>
-    <input type="password" name="password_check" id="password_check" /><br/>
-    <input type="submit"/>
-  </p>
-</form>
+<body>
+  <div class="container">
+    <section>
+      <class="row">
+        <form action="inscription_post.php" method="post" class="form-mise-en-page col-lg-6 form-horizontal">
+          <p>
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" class="form-control"/>
+            <label for="mail">Email :</label>
+            <input type="text" name="mail" id="mail" class="form-control"/>
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" class="form-control"/>
+            <label for="password_check">Répétez le mot de passe :</label>
+            <input type="password" name="password_check" id="password_check" class="form-control"/>
+            <br/>
+            <input type="submit" class="form-control"/>
+          </p>
+        </form>
+      </class>
+    </section>
+  </div>
+  <?php require('html_parts/charger_CDN_bootstrap.html'); ?>
+</body>
