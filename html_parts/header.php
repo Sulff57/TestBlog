5a1573a6fbf273b31d65e152181a6187c4442be5
Sulff <valo.brice@gmail.com>
2019-05-16 23:08:16 +0200

    modifications header / bannière

--- html_parts/header.php
+++ html_parts/header.php
@@ -1,21 +1,30 @@
-<header id="menu_principal" class="row align-items-center">
-    <h1 class="col-md-2">
-      <a href="index.php"><img src="images/logos/logo_TestBlog.png"/></a>
-    </h1>
-    <nav class="col-lg-3">
-      <ul class="nav">
-        <li class="nav-item align-self-center"><a class="nav-link" href="http://www.bvalo.fr/cv">CV</a></li>
-        <li class="nav-item align-self-center"><a class="nav-link" href="https://github.com/Sulfz/TestBlog">Git</a></li>
-      </ul>
-    </nav>
-    <section id="login" class="col-md-3 offset-md-4">
-      <span id="session"><?php
+<header>
+  <nav class="navbar navbar-expand-sm fixed-top" id="top_menu_banniere">
+    <ul class="navbar-nav" id="zone_navigation">
+      <li class="nav-item">
+        <a class="navbar-brand" href="index.php"><img src="images/logos/logo_TestBlog.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://www.bvalo.fr/cv">CV</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/Sulfz/TestBlog">Git</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto" id="zone_login">
+      <li class="nav-item ml-auto"><?php
         if (verif_session()) {
-          echo '<a href="profil.php">'. verif_session() . '</a><a href="deconnexion.php"><i class="fas fa-sign-out-alt" id="icone_logout"></i></a>';
+          echo '<a class="nav-link" href="profil.php">'. verif_session() . '</a>';
+          echo '</li>';
+          echo '<li>';
+          echo '<a class="nav-link" href="deconnexion.php"><i class="fas fa-sign-out-alt" id="icone_logout"></i></a>';
+          echo '</li>';
         }
         else {
-          echo '<i class="fas fa-user" id="icone_login"></i><a href="connexion.php">Connexion / Inscription</a>';
+          echo '<li>';
+          echo '<a class="nav-link" href="connexion.php"><i class="fas fa-user" id="icone_login"></i>Connexion / Inscription</a>';
+          echo '</li>';
         }
       ?></span>
-    </section>
+    </ul>
 </header>
